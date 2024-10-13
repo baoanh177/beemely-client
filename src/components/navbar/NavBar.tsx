429bfded9d10ab8052fcfc37155df5a207979364
PhongLB_2004 <luongphong.dev@gmail.com>
2024-10-13 15:33:32 +0700

    u

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -10,9 +10,9 @@ const Navbar = () => {
   return (
     <nav className="w-full bg-white-500">
       <div className="flex items-center justify-between font-medium">
-        <div className="z-50 flex w-full justify-between p-5 md:w-auto">
-          <img src={Logo} alt="logo" className="h-9 md:cursor-pointer" />
-          <div className="flex items-center md:hidden">
+        <div className="z-50 flex w-full justify-between p-5 lg:w-auto">
+          <img src={Logo} alt="logo" className="h-9 cursor-pointer" />
+          <div className="flex items-center lg:hidden">
             <SearchOutlined className="mr-4 text-lg" />
             <HeartOutlined className="mr-4 text-lg" />
             <ShoppingCartOutlined className="mr-4 text-lg" />
@@ -21,21 +21,23 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        <ul className="hidden items-center justify-between gap-8 font-[Poppins] uppercase md:flex">
+        <ul className="hidden items-center justify-between gap-8 font-[Poppins] uppercase lg:flex">
           <NavLinks />
         </ul>
 
-        <div className="hidden items-center md:flex">
+        <div className="hidden items-center lg:flex">
           <SearchOutlined className="mr-4 text-lg" />
           <HeartOutlined className="mr-4 text-lg" />
           <ShoppingCartOutlined className="mr-4 text-lg" />
-          <div className="hidden md:block">
+          <div className="hidden lg:block">
             <ButtonLogin />
           </div>
         </div>
-        {/* Mobile nav */}
+        {/* Mobile and Tablet nav */}
         <ul
-          className={`fixed bottom-0 top-0 z-40 w-full overflow-y-auto bg-white-500 py-24 pl-4 duration-500 md:hidden ${open ? "left-0" : "left-[-100%]"} `}
+          className={`fixed bottom-0 top-0 z-40 w-full overflow-y-auto bg-white-500 py-24 pl-4 duration-500 lg:hidden ${
+            open ? "left-0" : "left-[-100%]"
+          }`}
         >
           <NavLinks />
           <div className="py-5">
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,10 @@
-import { Container } from "@/styles/common-styles";
-// import NavBarAntd from "@/components/navbar-antd/Navbar";
-import BannerSlider from "@/components/banner/Banner";
-import Navbar from "@/components/navbar/NavBar";
-import Footer from "@/components/footer/footer";
+import DefaultLayout from "@/layouts/Default";
 
 const Home = () => {
   return (
-    <header>
-      {/* <section className="navbar-antd">
-        <Container>
-          <NavBarAntd />
-        </Container>
-      </section> */}
-      <section className="navbar">
-        <Container>
-          <Navbar />
-        </Container>
-      </section>
-      <section className="banner-slide px-8">
-        <BannerSlider />
-      </section>
-      <section className="footer">
-        <Footer />
-      </section>
-    </header>
+    <>
+      <DefaultLayout />
+    </>
   );
 };
 
