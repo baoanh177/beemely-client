c296bf7b769a5e6f21e1970fb74765e8f34ad449
nguyenthihien <hienn78333@gmail.com>
2024-11-12 20:26:23 +0700

    fix/review-product-createReview

--- src/services/store/review/review.thunk.ts
+++ src/services/store/review/review.thunk.ts
@@ -1,6 +1,7 @@
 import { client } from "@/services/config/client";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { IReview } from "./review.model";
+import { IThunkPayload } from "@/shared/utils/shared-interfaces";
 
 const prefix = "/api/client/reviews";
 
@@ -15,10 +16,8 @@ export const getAllReviews = createAsyncThunk("review/get-all-reviews", async (_
 
 export const createReview = createAsyncThunk("review/create-review", async (payload: IReview, { rejectWithValue }) => {
   try {
-    console.log("pasddvf", payload);
-
-    const { response, data } = await client.post(prefix, payload);
-
+    const thunkPayload: IThunkPayload = { body: payload };
+    const { response, data } = await client.post(prefix, thunkPayload);
     return response.status >= 400 ? rejectWithValue(data) : data;
   } catch (error: any) {
     return rejectWithValue(error.response.data);
